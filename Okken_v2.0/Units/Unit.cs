1bff6546decdccf1c3c95cfabe64c130158847de
Alexandr Tolstov <SESA574650@se.com>
2021-01-31 17:12:57 +0600

    Units

---
+++ Okken_v2.0/Units/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okken
+{
+    class Unit
+    {
+        public string Type { get; }
+        public string Name { get; set; }
+        public int? NumOfPole { get; set; }
+        public string TypeOfBreakingCapacity { get; set; }
+        public int? ShortСircuitСurrent { get; set; }
+        public int? RatedСurrent { get; set; }
+        public int? NumOfUnit { get; set; }
+        public string Description { get; set; }
+        public double? PriceOfUnit { get; set; }
+    }
+}
